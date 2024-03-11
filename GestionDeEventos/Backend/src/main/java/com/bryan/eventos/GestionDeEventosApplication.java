ad90e9c722974f79145b7c828fdd186582046ce8
Bryan <bryanpool991@hotmail.com>
2024-03-11 12:54:58 -0500

    first commit

---
+++ GestionDeEventos/Backend/src/main/java/com/bryan/eventos/GestionDeEventosApplication.java
@@ -0,0 +1,13 @@
+package com.bryan.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeEventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeEventosApplication.class, args);
+	}
+
+}
