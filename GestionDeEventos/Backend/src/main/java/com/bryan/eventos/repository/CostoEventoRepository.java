cc4dce152295f4614f79291ef2059464d66305a3
Bryan <bryanpool991@hotmail.com>
2024-03-12 18:39:42 -0500

    creacion de repository de entidad costo

---
+++ GestionDeEventos/Backend/src/main/java/com/bryan/eventos/repository/CostoEventoRepository.java
@@ -0,0 +1,7 @@
+package com.bryan.eventos.repository;
+
+import com.bryan.eventos.entity.CostoEvento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CostoEventoRepository extends CrudRepository<CostoEvento,Long> {
+}
