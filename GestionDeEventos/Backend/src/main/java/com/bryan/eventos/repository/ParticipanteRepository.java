b9964b972e24eacd52417f3dd4ac138989a0f662
Bryan <bryanpool991@hotmail.com>
2024-03-13 11:30:22 -0500

    repository de participante listo

---
+++ GestionDeEventos/Backend/src/main/java/com/bryan/eventos/repository/ParticipanteRepository.java
@@ -0,0 +1,7 @@
+package com.bryan.eventos.repository;
+
+import com.bryan.eventos.entity.Participante;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ParticipanteRepository extends CrudRepository<Participante,Long> {
+}
