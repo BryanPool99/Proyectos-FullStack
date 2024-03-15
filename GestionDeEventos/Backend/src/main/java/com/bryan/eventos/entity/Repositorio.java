28f6cefd39e42e28b58ef95e2cf4e0d489d6cc6c
Bryan <bryanpool991@hotmail.com>
2024-03-15 12:58:52 -0500

    update entity Repositorio

--- GestionDeEventos/Backend/src/main/java/com/bryan/eventos/entity/Repositorio.java
+++ GestionDeEventos/Backend/src/main/java/com/bryan/eventos/entity/Repositorio.java
@@ -26,5 +26,10 @@ public class Repositorio {
     @Column(columnDefinition = "DATE")
     private LocalDate fecha;
     @OneToMany(mappedBy = "repositorio", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
     private List<Evento> eventos;
+    // New constructor to handle ID deserialization from JSON
+    public Repositorio(int id) {
+        this.id = (long) id; // Cast to Long for compatibility with the field type
+    }
 }
